3c79ee842918bf8d98023dc9155001c667eeafda
Keppelen <keppelen.as@gmail.com>
2013-01-13 22:33:55 -0200

    remvoe sites velhos

--- Imprensa/informativo/sites/frontinbh.com.br/2011/certificado/conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Conecta-se com o MySQL
-mysql_connect("localhost", "bygio431_front", "frontinbh");
-
-// Seleciona banco de dados
-mysql_select_db("bygio431_frontinbh");
-?>
--- Imprensa/informativo/sites/planejamento_corte/conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Conecta-se com o MySQL
-mysql_connect("localhost", "bygio431_gio", "335621");
-
-// Seleciona banco de dados
-mysql_select_db("bygio431_modularizacao");
-?>

d9a0554df78aaab7e399002e0deebb3b7a5fac66
keppelen <keppelen.as@gmail.com>
2013-02-08 13:50:31 -0200

    add header 2013

--- certificado/certificado-head.php
+++ certificado/certificado-head.php
@@ -5,15 +5,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <![endif]-->
 
-  <meta name="description" content="O Front in BH 2012 tem foco em desenvolvimento Front-end e o evento acontecerá em Maceió no dia 27 de Outubro no auditório principal do CESMAC">
-  <<meta property="og:title" content="FrontInBH 2012" />
-  <meta property="og:type" content="article"/>
-	<meta property="og:description" content="O FrontInBH chega a sua segunda edição com um passado de sucesso. Reunirá a comunidade frontend de minas gerais e trará grandes palestrantes do cenário nacional." />
-	<meta property="og:url" content="http://frontinbh.com.br/"/>
-	<meta property="og:image" content="http://frontinbh.com.br/facebook/facebook.png" />
-	<meta property="og:site_name" content="FrontInBH"/>
-	<meta property="og:locale" content="pt_br" />
-	<meta property="fb:app_id" content="460492653966145" />
+<meta name="description" content="O Front in BH 2012 tem foco em desenvolvimento Front-end e o evento acontecerá em Maceió no dia 27 de Outubro no auditório principal do CESMAC">
+<meta property="og:title" content="FrontInBH 2012" />
+<meta property="og:type" content="article"/>
+<meta property="og:description" content="O FrontInBH chega a sua segunda edição com um passado de sucesso. Reunirá a comunidade frontend de minas gerais e trará grandes palestrantes do cenário nacional." />
+<meta property="og:url" content="http://frontinbh.com.br/"/>
+<meta property="og:image" content="http://frontinbh.com.br/facebook/facebook.png" />
+<meta property="og:site_name" content="FrontInBH"/>
+<meta property="og:locale" content="pt_br" />
+<meta property="fb:app_id" content="460492653966145" />
 
   <link rel="stylesheet" href="../css/main.css">
   <link rel="stylesheet" href="css/certificado.css">

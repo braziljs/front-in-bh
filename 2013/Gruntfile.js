bd70fe941fad6b3ae1a5878b072486d6e82251fb
Rodolfo Dias <rmdias29@gmail.com>
2013-07-23 17:13:16 -0300

    apagando arquivos que não são usados

--- 2013/Gruntfile.js
+++ 2013/Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function( grunt ) {
             watch : {
                 dist : {
                     files : [
-                        'src/assets/**/*'
+                        'src/**/*'
                     ],
                     tasks : [ 'uglify', 'cssmin']
                 }
